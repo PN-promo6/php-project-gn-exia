c80ecbf3e222f44413ab0bda591a102dd9477f22
gn-exia <djamelmiloudi3@gmail.com>
2020-04-23 11:44:40 +0200

    bootstrap fix

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@
   <title>Publish your best deck</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
 
 </head>
 
---
+++ src/Entity/User.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Entity;
+
+class User {
+    public $id;
+    public $nickname;
+    public $password;
+}
